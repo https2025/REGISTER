bf177145d3458c0c117152af28cd78cd597f7eac
PT GIMA HOSTING INDONESIA <yohanesmatius509@gmail.com>
2025-07-05 19:37:26 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Ganti dengan data dari Firebase project kamu
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
